65ffbff6487c9fdfb20c7cd247421e6670384227
yudadananjaya <dananjayayuda@gmail.com>
2025-01-08 16:36:24 +0700

    update pom file

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # altonaut-web-ui-automation
+
+## Running Tests
+
+### Prerequisites
+- Java Development Kit (JDK)
+- Maven or Gradle
+- WebDriver binaries (e.g., ChromeDriver, GeckoDriver)
+
+### Running Tests in Headless Mode
+
+To run the tests in headless mode, ensure that the `Config.BROWSER` is set to either `chrome` or `firefox`. The `BaseTest` class has been configured to run in headless mode by default.
+
+#### Using Maven
+```sh
+mvn clean install
+mvn test
--- src/main/java/com/altonaut/base/BaseTest.java
+++ src/main/java/com/altonaut/base/BaseTest.java
@@ -2,43 +2,52 @@ package com.altonaut.base;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
-
+import org.openqa.selenium.firefox.FirefoxOptions;
 import io.github.bonigarcia.wdm.WebDriverManager;
-
 import java.io.IOException;
-
 import com.altonaut.modules.login.pageActions.LoginPageActions;
 import com.altonaut.modules.login.assertions.LoginAssertions;
 import com.altonaut.config.Config;
 
-public class LoginBaseTest {
+public class BaseTest {
     protected WebDriver driver;
     protected LoginPageActions loginPageActions;
     protected LoginAssertions loginAssertions;
 
-    public void setUp() throws IOException{
+    public void setUp() throws IOException {
         // Fetch browser type and base URL from Config
         String browser = Config.BROWSER;
         String baseUrl = Config.BASE_URL;
 
         // Set up the driver based on the browser specified in the Config class
-        if ("chrome".equalsIgnoreCase(browser)) {
-            WebDriverManager.chromedriver().setup();
-            driver = new ChromeDriver();
-        } else if ("firefox".equalsIgnoreCase(browser)) {
-            WebDriverManager.firefoxdriver().setup();
-            driver = new FirefoxDriver();
-        } else {
-            throw new IllegalArgumentException("Unsupported browser: " + browser);
-        }
-
-        // Maximize browser window and navigate to base URL
+        driver = initializeDriver(browser);
         driver.manage().window().maximize();
         driver.get(baseUrl);
 
         loginPageActions = new LoginPageActions();
         loginAssertions = new LoginAssertions();
+    }
+
+    private WebDriver initializeDriver(String browser) {
+        if ("chrome".equalsIgnoreCase(browser)) {
+            WebDriverManager.chromedriver().setup();
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--headless");
+            options.addArguments("--disable-gpu");
+            options.addArguments("--window-size=1920,1080");
+            return new ChromeDriver(options);
+        } else if ("firefox".equalsIgnoreCase(browser)) {
+            WebDriverManager.firefoxdriver().setup();
+            FirefoxOptions options = new FirefoxOptions();
+            options.addArguments("--headless");
+            options.addArguments("--disable-gpu");
+            options.addArguments("--window-size=1920,1080");
+            return new FirefoxDriver(options);
+        } else {
+            throw new IllegalArgumentException("Unsupported browser: " + browser);
+        }
     }
 
     public void tearDown() {
